f5b33a9970ec5ad4a51c8cefd68e4ac4e8e5385a
raisolomon2012@gmail.com <raisolomon2012@gmail.com>
2019-09-25 15:25:41 +0200

    intial commit

--- code/locatie.py
+++ code/locatie.py
@@ -1,5 +1,14 @@
 class Locatie:
 
-    def __init__(self, ID, naam):
+    def __init__(self, ID, naam, omschrijving,locked, 
+                unlock_omschrijving, objecten, verbonden_met, win):
         self.ID = ID
         self.naam = naam
+        self.omschrijving = omschrijving
+        self.locked = locked
+        self.unlock_omschrijving = unlock_omschrijving
+        self.objecten = objecten
+        self.verbonden_met = verbonden_met
+        self.win = win
+    
+    
