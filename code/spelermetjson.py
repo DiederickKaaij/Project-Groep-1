80f6adc0ab18cc105d977f688681002e86790af7
Diederick Kaaij <diederickkaaij@gmail.com>
2019-09-26 10:38:58 +0200

    made speler class

--- code/spelermetjson.py
+++ code/spelermetjson.py
@@ -1,2 +1,19 @@
 from commandmetjson import Command
 
+class Speler:
+
+    def __init__(self, huidigeLocatie, inventory):
+        self.huidigeLocatie = huidigeLocatie
+        self.inventory = inventory
+    
+    def command(self, commando):
+        self.happening = Command(commando, self.huidigeLocatie, self.inventory)
+        output = self.happening.isCommando()
+
+        if isinstance(output, str):
+            self.inventory.append(output)
+        elif output > 0:
+            self.huidigeLocatie = output
+        
+    def isWinst(self):
+        return self.happening.locatie.locaties[self.huidigeLocatie-1]['win']
